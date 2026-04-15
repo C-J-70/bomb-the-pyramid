5ed658574643b4e3b6f57e2c00f803417757666d
C-J-70 <78919494+C-J-70@users.noreply.github.com>
2026-04-15 15:18:10 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ images.g.ts
@@ -0,0 +1,70 @@
+// Automatisch generierter Code. Nicht bearbeiten.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "pyramid":return img`
+................................
+..............4.................
+.............444................
+............44444...............
+...........4444444..............
+..........444444444.............
+.........44444444444............
+........4444444444444...........
+.......444444444444444..........
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+`;
+            case "image2":
+            case "bomb":return img`
+. . . . . . . . . 4 . . . . . . 
+. . . . . . . . f . . . . . . . 
+. . . . 8 8 8 8 8 8 8 8 8 . . . 
+. . . 8 1 1 8 8 8 8 8 8 8 8 . . 
+. . . 8 1 1 8 8 8 8 8 8 8 8 . . 
+. . . 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . . 8 8 8 8 8 8 8 8 8 . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Automatisch generierter Code. Nicht bearbeiten.
